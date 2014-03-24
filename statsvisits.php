4185cf8b11e66faef90e2d13bea802c2c00034be
Quetzacoalt91 <thomas.nabord@prestashop.com>
2014-03-24 14:59:28 +0100

    / MO statsvisits : ps_versions_compliancy added

--- statsvisits.php
+++ statsvisits.php
@@ -36,7 +36,7 @@ class StatsVisits extends ModuleGraph
 	{
 		$this->name = 'statsvisits';
 		$this->tab = 'analytics_stats';
-		$this->version = 1.0;
+		$this->version = 1.2;
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
@@ -44,6 +44,7 @@ class StatsVisits extends ModuleGraph
 
 		$this->displayName = $this->l('Visits and Visitors');
 		$this->description = $this->l('Adds statistics about your visits and visitors to the Stats dashboard.');
+		$this->ps_versions_compliancy = array('min' => '1.5.6.1', 'max' => _PS_VERSION_);
 	}
 
 	public function install()
