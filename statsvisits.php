2f50ba52230891840de3f9f4358d728c7f89afdb
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-08-05 09:28:28 +0200

    // Version updated to v1.6.0

--- statsvisits.php
+++ statsvisits.php
@@ -36,7 +36,7 @@ class StatsVisits extends ModuleGraph
 	{
 		$this->name = 'statsvisits';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.5.1';
+		$this->version = '1.6.0';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
